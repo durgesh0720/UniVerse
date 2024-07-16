86364cb544205e60276f54cb5520837c2026b9c7
durgesh0720 <jarvisjs283@gmail.com>
2024-07-16 13:22:02 +0530

    Universe-Update

--- UniVerse/UniVerseProject/accounts/views.py
+++ UniVerse/UniVerseProject/accounts/views.py
@@ -24,39 +24,53 @@ def login(request):
     d1={}
     try:
         if request.GET['error']==str(1):
-            d1['errmsg']="Username and password dosen't matched"
+            d1['errmsg']="Username and password dosen't matched "
     except:
         d1['errmsg']=''
     return render(request,"login.html",d1)
 
 @csrf_protect
 def saveUsers(request):
-    if request.method=="POST":
-        logger.debug(f"CSRF cookie: {request.COOKIES.get('csrftoken')}")
-        logger.debug(f"CSRF token in POST: {request.POST.get('csrfmiddlewaretoken')}")
+    try:
+        if request.method=="POST":
+            logger.debug(f"CSRF cookie: {request.COOKIES.get('csrftoken')}")
+            logger.debug(f"CSRF token in POST: {request.POST.get('csrfmiddlewaretoken')}")
 
-        user=student_registration()
-        check_username_exists=student_registration.objects.filter(username=request.POST['username'])
-        if not check_username_exists:
-            user.firstname=request.POST['firstname']
-            user.lastname=request.POST['lastname']
-            user.email=request.POST['email']
-            user.username=request.POST['username']
-            user.password=request.POST['password']
-            user.save()
-            url="http://localhost:8000/welcome"
-        else:
-            url="http://localhost:8000/signup?error=1"
-        return redirect(url)
-    
-    return redirect('signup')
+            user=student_registration()
+            check_username_exists=student_registration.objects.filter(username=request.POST['username'])
+            if not check_username_exists:
+                user.firstname=request.POST['firstname']
+                user.lastname=request.POST['lastname']
+                user.email=request.POST['email']
+                user.username=request.POST['username']
+                user.password=request.POST['password']
+                user.save()
+                context={
+                    'firstname':request.POST['firstname'],
+                    'lastname':request.POST['lastname'],
+                }
+                return render(request,"Welcome.html",context)
+            else:
+                url="http://localhost:8000/signup?error=1"
+            return redirect(url)
+    except:
+        return redirect('homepage')
 
 def checkUser(request):
-    username=request.POST['username']
-    password=request.POST['password']
-    is_student=student_registration.objects.filter(username=username,password=password)
-    if not is_student:
+    try:
+        username=request.POST['username']
+        password=request.POST['password']
+        is_student=student_registration.objects.filter(username=username,password=password)
+        if not is_student.exists():
+            url = "http://localhost:8000/login?error=1"
+            return redirect(url)
+        else:
+            student = is_student.values().first()
+            context = {
+                'firstname': student['firstname'],
+                'lastname': student['lastname'],
+            }
+            return render(request,"Welcome.html",context) 
+    except:
         url="http://localhost:8000/login?error=1"
         return redirect(url)
-    else:
-        return render(request,"Welcome.html",{"user":username})
